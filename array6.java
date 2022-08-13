48aacab007741960a61d81f466a1d646bcac9354
kaustubhgondhali <110551976+kaustubhgondhali@users.noreply.github.com>
2022-08-13 10:00:18 +0530

    Add files via upload

---
+++ array6.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class array6
+{
+public static void main(String args [])
+{
+int[][] kaustubh = new int[2][2];
+int[][] b = {{1,2,3,},{6,7,8},{4,3,8}};
+arr[0][1] = 150;
+arr[1][0] = 10;
+System.out.println("arrary b");
+for(int i=0; i<b.length;i++){
+// for the row
+for(int j=0; j<b[i].length; j++)
+{
+System.out.print(b[i][j]);
+}
+System.out.println();
+}
+}
+}
